6fde8349caddc1262dfcf1d45e134634f4244aab
GiordanoGuidi <giordanoguidi23@gmail.com>
2023-12-21 15:43:45 +0100

    recovered items

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log('JS OK')
+
+
+//RECUPERO ELEMENTI
+
+const userElement = document.getElementById('user-cube')
+console.log(userElement)
+const computerElement = document.getElementById('computer-cube')
+console.log(computerElement)
+const victoryElement = document.getElementById('victory')
+console.log(victoryElement)
+
+
