43c4681e8a69225d958ea056110f792bce5c60cb
GiordanoGuidi <giordanoguidi23@gmail.com>
2023-12-21 16:00:15 +0100

    added variables

--- js/script.js
+++ js/script.js
@@ -16,3 +16,22 @@ console.log(userNumber)
  const computerNumber = Math.floor((Math.random() * 6) + 1);
  console.log(computerNumber)
 
+// SCRIVO RISULTATO IN PAGINA
+userElement.innerText =  ' Il numero dell\'utente è : ' + userNumber
+computerElement.innerText = ' Il numero del computer è : ' + computerNumber;
+
+// AGGIUNGO LA VARIABILE
+let message = 'Parità'
+victoryElement.innerText= message
+
+if( userNumber < computerNumber){
+    message = ' Il computer ha vinto ' + computerNumber
+    victoryElement.innerText = message
+    console.log(victoryElement)
+} 
+else if(userNumber > computerNumber){
+    message = ' L\'utente ha vinto ' + userNumber
+    victoryElement.innerText = message
+}
+        
+    
